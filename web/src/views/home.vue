598ee717a3b791c1c42256919cbaa71497f5de59
puzzle <happypuzzle@126.com>
2023-02-03 19:14:32 +0800

    doc how to use

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -52,9 +52,8 @@
                 </n-form-item>
               </n-dynamic-input>
             </n-form>
-            <br>
-            <n-button type="info" attr-type="button" :loading="tunnel_loading" @click="tunnelSubmit" block>
-              保存
+            <br/>
+            <n-button type="info" attr-type="button" :loading="tunnel_loading" @click="tunnelSubmit" block>保存
             </n-button>
           </n-tab-pane>
         </n-tabs>
@@ -63,8 +62,22 @@
 
     <br/>
     <!-- prettier-ignore-->
-    <n-card title="使用方法" class="info" hoverable v-show="tab_name == 'tunnel'">
-      <div>
+    <n-card title="使用方法" class="use" hoverable>
+      <div v-show="tab_name == 'info'">
+        <p>下载客户端软件:</p>
+        <ul>已有 NPS 可以略过此部分</ul>
+        <ul v-for="(urls, system_type) in downloads">
+          <b>{{ system_type }}:</b>
+          <n-button v-for="(download_url, download_name) in urls" tertiary type="info" size="small" tag="a" :href="download_url" target="_blank">
+            {{ download_name }} 下载
+          </n-button>
+        </ul>
+
+        <p>解压后在当前目录打开终端 并 输入连接命令 进行连接 如</p>
+        <ul><img src="/npc_linux.png" alt="linux connect"/></ul>
+      </div>
+
+      <div v-show="tab_name == 'tunnel'">
         <p>隧道类型:</p>
         <ul v-for="info in tunnel_types" :key="info.value"><b>{{ info.label }}</b>: {{ info.placeholder }}</ul>
         <p>服务端端口:</p>
@@ -190,10 +203,19 @@ const tunnel = ref<FormInst | null>(null),
             tunnel_loading.value = false
           })
     }
+
+const downloads = {
+  win64: {
+    github: 'https://github.com/ehang-io/nps/releases/download/v0.26.10/windows_amd64_client.tar.gz',
+  },
+  linux64: {
+    github: 'https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_amd64_client.tar.gz',
+  },
+}
 </script>
 
 <style lang="stylus" scoped>
-.info
+.info, .use
   width 98%
   max-width 800px
   margin auto
@@ -204,4 +226,7 @@ const tunnel = ref<FormInst | null>(null),
     color: #ca4440;
     font-size: 90%;
     padding: 2px 4px;
+
+  img
+    width 100%
 </style>
